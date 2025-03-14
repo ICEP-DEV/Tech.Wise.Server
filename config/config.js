c448c41bdf2497bf903ad108bfc979391314821c
kabelo-mastermind <0604749776kb@gmail.com>
2025-03-14 12:00:31 +0200

    removed acquireTimeout from trips endpoint

--- config/config.js
+++ config/config.js
@@ -7,10 +7,12 @@ const pool = mysql.createPool({
   user: 'sql8766593',
   password: 'TZzNrUc4fB',
   database: 'sql8766593',
-  waitForConnections: true,
-  queueLimit: 0,
-  port: 3306
+  waitForConnections: true,  // Ensures the pool waits for a connection to be released before throwing an error
+  queueLimit: 0,            // No limit to the queue
+  port: 3306,
+  connectTimeout: 10000,     // Connection timeout in ms (10 seconds)
 });
+
 
 // Handle connection errors globally
 pool.on('error', (err) => {
