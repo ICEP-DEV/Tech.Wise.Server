f8511bd3003a2c0659e27d62b927204670242db7
kabelo-mastermind <0604749776kb@gmail.com>
2025-03-22 12:19:30 +0200

    latest trip status for a specific user

--- routes/trips.js
+++ routes/trips.js
@@ -323,6 +323,7 @@ router.put('/trips/:tripId/status', async (req, res) => {
 });
 // Endpoint to fetch the latest trip status for a specific user
 router.get('/trips/statuses/:userId', async (req, res) => {
+    console.log('Fetching latest trip status for user:', req.params);
     const { user_id } = req.params;
 
     if (!user_id) {
