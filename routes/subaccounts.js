54a8133ec8a30386d06d2c97d54f8554fdf9fdfb
Rebotilwe <rebotilwemokiba@gmail.com>
2025-03-28 10:29:30 +0200

    Default value Create subaccount endpoint

--- routes/subaccounts.js
+++ routes/subaccounts.js
@@ -9,7 +9,7 @@ const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
 // Create subaccount endpoint
 router.post("/create-subaccount", async (req, res) => {
     const { business_name, settlement_bank, account_number } = req.body;
-    const percentage_charge = '100'; // Default value
+    const percentage_charge = '3'; // Default value
     if (!business_name || !settlement_bank || !account_number || !percentage_charge) {
         return res.status(400).json({ error: "All fields are required" });
     }
