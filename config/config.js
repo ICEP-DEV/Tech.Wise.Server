209b6647f3b255ed5da6f73e6d5a85a44a615ba1
Mastermind <81708008+kabelo-mastermind@users.noreply.github.com>
2025-03-29 13:39:01 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise'); // Use mysql2 with promises
 // Create a connection pool
 const pool = mysql.createPool({
   connectionLimit: 10, // Adjust as needed
-  host: 'sql3.freesqldatabase.com',
-  user: 'sql3767836',
-  password: 'DfaCTHkGMH',
-  database: 'sql3767836',
+  host: 'sql8.freesqldatabase.com',
+  user: 'sql8770262',
+  password: '3EEGCWaNnH',
+  database: 'sql8770262',
   waitForConnections: true,  // Ensures the pool waits for a connection to be released before throwing an error
   queueLimit: 0,            // No limit to the queue
   port: 3306,
