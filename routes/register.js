9afeca3cc717e4b650a3aa3f43ec5b466dc361ec
kabelo-mastermind <0604749776kb@gmail.com>
2025-05-05 15:23:14 +0200

    Endpoint to register a new user

--- routes/register.js
+++ routes/register.js
@@ -3,20 +3,21 @@ const router = express.Router();
 const pool = require("../config/config"); // Import the MySQL connection pool
 require("dotenv").config();
 
+// Endpoint to register a new user
 router.post('/register', async (req, res) => {
     console.log('Received request:', req.body);
 
     try {
-        const { name, email, password, role, gender, user_uid } = req.body;
+        const { name, email, password, role, user_uid } = req.body;
 
-        if (!name || !email || !password || !role || !gender || !user_uid) {
+        if (!name || !email || !password || !role || !user_uid) {
             return res.status(400).json({ message: "All fields are required" });
         }
 
-        const sql = `INSERT INTO users (name, email, password, role, gender, user_uid) VALUES (?, ?, ?, ?, ?, ?)`;
+        const sql = `INSERT INTO users (name, email, password, role, user_uid) VALUES (?, ?, ?, ?, ?)`;
 
         const connection = await pool.getConnection();
-        const [result] = await connection.execute(sql, [name, email, password, role, gender, user_uid]);
+        const [result] = await connection.execute(sql, [name, email, password, role, user_uid]);
         connection.release(); // Release the connection back to the pool
 
         res.status(201).json({ message: 'User registered successfully', userId: result.insertId });
